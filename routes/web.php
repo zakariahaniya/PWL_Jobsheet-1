f5deb95440f4314cbb18651bc1e466ccafc86ada
MuhammadZakariaHaniya <zakalvha@gmail.com>
2026-03-30 11:37:45 +0700

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,35 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('Laravel siap digunakan 🚀');
 });
+
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return "World";
+});
+
+Route::get('/', function () {
+    return "Selamat Datang";
+});
+
+Route::get('/about', function () {
+    return "NIM : 244107020135 <br> Nama : Muhammad Zakaria Haniya";
+});
+
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya '.$name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID '.$id;
+});
+
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama saya '.$name;
+});
